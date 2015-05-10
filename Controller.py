6f9dd5caee5ca629eea28de153a695171412d138
georgeGitM <zhiz2@student.unimelb.edu.au>
2015-05-11 08:42:18 +1000

    Update controller

--- Controller.py
+++ Controller.py
@@ -1,37 +1,20 @@
 from flask import Flask
 from flask.ext import restful
 from flask import render_template
-import urllib2
+import urllib
+import requests
 import Model
 import couchdb
+import json
 
 app = Flask(__name__)
 api = restful.Api(app)
 
-
-@app.route('/')
-def index():
-    city = 'Toronto'
-    result = make_request('localhost:5000/Api/Get_All_Document')
-    return render_template('Index.html', city = city, rows = rows)
-
-
-def make_request(url):
-    url = url
-    try:
-        response = urllib2.urlopen(url).read()
-        print(response)
-    except urllib2.HTTPError as e:
-        print(e.message)
-
-
-def count_scenario():
-    return 3
 db = Model.setup_couchdb('')
 
 class Documents(restful.Resource):
     def get(self):
-        num_of_scenario = count_scenario()
+        num_of_scenario = 3
         map_fun = '''function(doc) {
             if(doc.id)
             emit(doc.id, doc);
@@ -41,5 +24,19 @@ class Documents(restful.Resource):
 
 api.add_resource(Documents, '/Api/Get_All_Document')
 
+@app.route('/')
+def index():
+    city = 'Toronto'
+    result = make_request('http://localhost:5000/Api/Get_All_Document')
+    #result = [1, 'foo']
+    return render_template('Index.html', city = city, result = json.dumps(result))
+
+
+def make_request(url):
+    url = url
+    response = requests.api.get(url)
+    result = response.json()
+    return result
+
 if __name__ == '__main__':
-    app.run(debug=api)
+    app.run(host='localhost' ,port=4444, debug=True)
